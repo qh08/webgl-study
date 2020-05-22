94afe8c3dea39d94ce2a56625d938aa4d6c4a4ff
qh08 <wuqihong90@gmail.com>
2020-05-22 11:44:49 +0800

    build: involve webpack-dev-server & multi-page config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,11 +1,31 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const glob = require("glob");
 
 const __root = path.resolve(__dirname, "../");
+const entryNames = glob
+  .sync("./src/examples/*/index.js")
+  .map((fileName) => fileName.match(/\.\/src\/examples\/(\S*)\/index\.js/)[1]);
+const entries = entryNames.reduce(
+  (prevObj, entryName) => ({
+    ...prevObj,
+    [`${entryName}/index`]: `./src/examples/${entryName}/index.js`,
+  }),
+  {}
+);
+const examplePages = entryNames.map(
+  (exampleName) =>
+    new HtmlWebpackPlugin({
+      title: exampleName,
+      filename: `${exampleName}/index.html`,
+      template: "./src/index.html",
+      chunks: [`${exampleName}/index`]
+    })
+);
 
 module.exports = {
-  entry: ["@babel/polyfill", "./src/dot/index.js"],
+  entry: entries,
   output: {
     path: path.resolve(__root, "dist"),
   },
@@ -24,10 +44,5 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/dot/index.html",
-    }),
-  ],
+  plugins: [new CleanWebpackPlugin(), ...examplePages],
 };
