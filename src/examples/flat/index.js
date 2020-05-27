4283d10891b4621a0a47b5d4c1cd2e06fdefc85c
qh08 <wuqihong90@gmail.com>
2020-05-27 22:19:33 +0800

    perf: remove second bindBuffer

--- src/examples/flat/index.js
+++ src/examples/flat/index.js
@@ -61,7 +61,6 @@ const coords = [
 const buffer = webglMaker.createBuffer();
 webglMaker.setBufferWithAttribute(buffer, aPosition, { size: 2, stride: 24, offset: 0 });
 webglMaker.setBufferWithAttribute(buffer, aColor, { size: 4, stride: 24, offset: 8 });
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coords), gl.DYNAMIC_DRAW);
 
 webglMaker.clear();
