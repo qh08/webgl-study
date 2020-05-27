39919d8b3c864991f3ecf2e8474dc7e11fa783b2
qh08 <wuqihong90@gmail.com>
2020-05-27 22:23:31 +0800

    feat: square with 4 dots

--- src/examples/flat/index.js
+++ src/examples/flat/index.js
@@ -38,18 +38,18 @@ const coords = [
   0,
   0,
   1, //V2
-  30 / screenWidth,
-  -30 / screenHeight,
-  0,
-  1,
-  0,
-  1, //V0
-  300 / screenWidth,
-  -300 / screenHeight,
-  0,
-  1,
-  0,
-  1, //V2
+  // 30 / screenWidth,
+  // -30 / screenHeight,
+  // 0,
+  // 1,
+  // 0,
+  // 1, //V0
+  // 300 / screenWidth,
+  // -300 / screenHeight,
+  // 0,
+  // 1,
+  // 0,
+  // 1, //V2
   300 / screenWidth,
   -30 / screenHeight,
   0,
@@ -58,10 +58,22 @@ const coords = [
   1, //V3
 ];
 
+var indices = [
+  0, 1, 2, //第一个三角形
+  0, 2, 3  //第二个三角形
+];
+
 const buffer = webglMaker.createBuffer();
+const indicesBuffer = webglMaker.createBuffer();
 webglMaker.setBufferWithAttribute(buffer, aPosition, { size: 2, stride: 24, offset: 0 });
 webglMaker.setBufferWithAttribute(buffer, aColor, { size: 4, stride: 24, offset: 8 });
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coords), gl.DYNAMIC_DRAW);
 
 webglMaker.clear();
-webglMaker.draw(gl.TRIANGLES, 0, coords.length / 6);
+// webglMaker.draw(gl.TRIANGLES, 0, coords.length / 6);
+
+gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
